bcbd94e6dc7cb4dcc91c111cc4d2dc74d85db86e
Krzysztof Jablonski <krzysztof.jablonski@swmansion.com>
2025-09-10 13:00:09 +0200

    add apps

--- expo-53/shared/automatedTests.tsx
+++ expo-53/shared/automatedTests.tsx
@@ -25,16 +25,10 @@ function printLogs() {
 
 export function AutomatedTests({ ws }: { ws: WebSocket | null }) {
   const style = useStyle();
+  const [elementVisible, setElementVisible] = useState(true);
 
   return (
-    <View
-      style={{
-        display: "flex",
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
+    <View style={style.mainContainer}>
       <View style={style.container}>
         <TrackableButton
           ws={ws}
@@ -62,6 +56,25 @@ export function AutomatedTests({ ws }: { ws: WebSocket | null }) {
             console.log("Response", response);
           }}
         />
+        <TrackableButton
+          ws={ws}
+          id="toggle-element-button"
+          title="Toggle element visibility"
+          onPress={() => {
+            console.log("Toggling element visibility");
+            setElementVisible((prev) => !prev);
+          }}
+        />
+        <View
+          style={{
+            marginTop: 20,
+            width: 50,
+            height: 50,
+            backgroundColor: "red",
+            display: elementVisible ? "flex" : "none",
+            margin: "auto",
+          }}
+        />
       </View>
     </View>
   );
@@ -70,7 +83,17 @@ export function AutomatedTests({ ws }: { ws: WebSocket | null }) {
 function useStyle() {
   const { gap, colors } = useScheme();
   return StyleSheet.create({
-    container: { gap: gap, backgroundColor: colors.background },
+    mainContainer: {
+      display: "flex",
+      flex: 1,
+      justifyContent: "center",
+      alignItems: "center",
+      backgroundColor: colors.background,
+    },
+    container: {
+      gap: gap,
+      backgroundColor: colors.background,
+    },
     stepContainer: { gap, marginHorizontal: gap * 4 },
   });
 }
